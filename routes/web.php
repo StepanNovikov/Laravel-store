40555b223244249deb51a48ca05357c7cd4bae54
Stepan <stepan.novikov.1996.com>
2021-01-09 15:14:06 +0300

    Added three first pages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +13,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@index');
+Route::get('/categories', 'MainController@categories');
+
+Route::get('/{category}', 'MainController@category');
+
+
+Route::get('/mobiles/{product?}', 'MainController@product');
